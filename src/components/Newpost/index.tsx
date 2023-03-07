65a3d7cdda49b1310f67e438292bd4d1051606ed
Arunkumar Selvaraj <arun@ioak.io>
2023-03-07 16:24:34 +0530

    New post page creation

--- src/components/Newpost/index.tsx
+++ src/components/Newpost/index.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { Input, Textarea } from 'basicui'
 import './style.scss';
+import { ArticleType } from 'reach';
 
 interface Props {
 }
 const Newpost = (props: Props) => {
 
+  const [state, setState] = useState<ArticleType.Article>({
+    title: "", description: "", views: 0, createdBy: "1", helpful: 0, notHelpful: 0, id: "", updatedBy: "", tags: []
+  });
+
   return (
     <div className="newpost">
-      new post
+      <form>
+        <Input value={state.title} name="title" label='Title' />
+        <Textarea value={state.description} name="description" label='Body' />
+      </form>
     </div>
   );
 };
