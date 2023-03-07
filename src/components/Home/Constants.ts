6fd2eb18f1f43fc53367c663031db07c73f3cd70
Arunkumar Selvaraj <arun@ioak.io>
2023-03-07 18:54:15 +0530

    New deployment version

--- src/components/Home/Constants.ts
+++
@@ -1,9 +0,0 @@
-export const IOAKBLOG_NUMBER_OF_BLOCKS = "IOAKBLOG_NUMBER_OF_BLOCKS";
-
-export const getCount = (index: number) => {
-    return `IOAKBLOG_COUNT_${index}`;
-}
-
-export const getType = (index: number) => {
-    return `IOAKBLOG_TYPE_${index}`;
-}
