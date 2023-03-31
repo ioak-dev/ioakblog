87a5a7c7a0cb8122f69524d7bbd4d986c6d7cbc9
manojvictor <fmanojvictor@gmail.com>
2023-03-31 14:31:10 +0530

    login and logout redirect fix

--- src/store/actions/AuthActions.ts
+++ src/store/actions/AuthActions.ts
@@ -1,4 +1,3 @@
-import { useNavigate } from 'react-router-dom';
 import { GET_AUTH, REMOVE_AUTH, ADD_AUTH } from './types';
 
 export const getAuth = () => (dispatch: any) => {
@@ -12,17 +11,10 @@ export const addAuth = (data: any) => (dispatch: any) => {
     type: ADD_AUTH,
     payload: data,
   });
-  console.log(data);
-  const navigate = useNavigate();
-  if (data.length > 0) {
-    navigate("/home");
-  }
 };
 
 export const removeAuth = () => (dispatch: any) => {
   dispatch({
     type: REMOVE_AUTH,
   });
-  const navigate = useNavigate();
-  navigate("/login");
 };
