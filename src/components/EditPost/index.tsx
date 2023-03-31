aba7215360d96f5da854679b19e66de1bede8599
Arunkumar Selvaraj <arun@ioak.io>
2023-03-31 12:58:27 +0530

    Integration of editor to edit and create post pages

--- src/components/EditPost/index.tsx
+++ src/components/EditPost/index.tsx
@@ -1,8 +1,36 @@
+import { useEditor } from '@tiptap/react';
 import { Button, Input, Textarea, ThemeType } from 'basicui';
 import { ArticleType, ArticleViewBodyChildWidget, ArticleViewMetadataChildWidget, ArticleViewTitleChildWidget, ArticleViewWidget, AuthorProfile } from 'reach';
 import React, { useEffect, useReducer, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { getEditorConfig } from '../../utils/EditorUtils';
+
+import {
+    Editor,
+    Bold,
+    Italic,
+    Strikethrough,
+    Underline as UnderlineComponent,
+    HeadingDropdown,
+    AlignmentDropdown,
+    AlignLeft,
+    AlignCenter,
+    AlignRight,
+    AlignJustify,
+    AddImage,
+    AddTable,
+    AddYoutubeVideo,
+    BlockQuote,
+    BulletList,
+    ClearFormatting,
+    Code,
+    CodeBlock,
+    FontColor,
+    HighlightColor,
+    HorizontalRule,
+    OrderedList
+} from 'writeup';
 import EditorComponent from '../EditorComponent';
 import { categories } from '../Home/data/CategoryData';
 import { getPostById } from '../ViewPost/service';
@@ -10,6 +38,7 @@ import { updatePost } from './service';
 import './style.scss';
 
 const EditPost = () => {
+    const editor = getEditorConfig();
     const navigate = useNavigate();
     const params = useParams();
     const [state, setState] = useState<ArticleType.Article>({
@@ -21,13 +50,21 @@ const EditPost = () => {
 
     useEffect(() => {
         if (params.id && authorization.isAuth) {
-            getPostById(params.id).then((data: any) => setState(data));
+            getPostById(params.id).then((data: any) => {
+                setState(data);
+                // editor?.commands.setContent(data.description)
+            });
         }
     }, [params, authorization]);
 
+    useEffect(() => {
+        console.log("***");
+        editor?.commands.setContent(state.description)
+    }, [state.description]);
+
     const save = () => {
         if (params.id) {
-            updatePost(params.id, state).then((data) => console.log(data));
+            updatePost(params.id, { ...state, description: editor?.getHTML() }).then((data) => console.log(data));
         }
     }
 
@@ -44,8 +81,32 @@ const EditPost = () => {
             <form>
                 <Input value={state.title} name="title" label='Title' onInput={handleChange} />
                 <Input value={state.featuredImage} name="featuredImage" label='Feature image' onInput={handleChange} />
-                <Textarea rows={5} value={state.description} name="description" label='Body' onInput={handleChange} />
-                <EditorComponent />
+                {/* <Textarea rows={5} value={state.description} name="description" label='Body' onInput={handleChange} /> */}
+                <Editor editor={editor}>
+                    <Bold editor={editor} />
+                    <Italic editor={editor} />
+                    <UnderlineComponent editor={editor} />
+                    <Strikethrough editor={editor} />
+                    <AlignLeft editor={editor} />
+                    <AlignCenter editor={editor} />
+                    <AlignRight editor={editor} />
+                    <AlignJustify editor={editor} />
+                    <HeadingDropdown editor={editor} />
+                    <AlignmentDropdown editor={editor} />
+                    <BulletList editor={editor} />
+                    <OrderedList editor={editor} />
+                    <BlockQuote editor={editor} />
+                    <Code editor={editor} />
+                    <CodeBlock editor={editor} />
+                    <FontColor editor={editor} />
+                    <HighlightColor editor={editor} />
+                    <HorizontalRule editor={editor} />
+                    <ClearFormatting editor={editor} />
+                    <AddImage editor={editor} imageUploadMethod="POST" imageUploadParam='file'
+                        imageUploadURL={`${process.env.REACT_APP_API_URL}/upload`} />
+                    <AddTable editor={editor} />
+                    <AddYoutubeVideo editor={editor} />
+                </Editor>
                 <div className="edit-post__action-bar">
                     <Button onClick={save} theme={ThemeType.primary}>Save changes</Button>
                     <Button onClick={cancel}>Cancel</Button>
